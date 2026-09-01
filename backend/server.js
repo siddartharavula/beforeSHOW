ef62f2fd5c707f1e2417c832d25158e6b6bc81a7
siddartharavula <siddartharavula2005@gmail.com>
2026-09-01 19:51:22 +0530

    Prepare backend for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const app = require("./app");
-const connectDB=require("./src/config/db");
+const connectDB = require("./src/config/db");
 
-PORT = process.env.PORT || 2005;
+const PORT = process.env.PORT || 2005;
 
 connectDB();
-app.listen(PORT, () => {
-  console.log(`Server Running At => http://localhost:${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
